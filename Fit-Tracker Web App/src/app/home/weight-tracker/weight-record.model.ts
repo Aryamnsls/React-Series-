509ae9ee8e187c0d5869d98ed467dfbb9a02ae91
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-10-26 23:58:15 +0530

    sad

--- Fit-Tracker Web App/src/app/home/weight-tracker/weight-record.model.ts
+++
@@ -1,9 +0,0 @@
-export interface IWeightRecord {
-  weight: number;
-  date: Date;
-}
-
-export class WeightRecord implements IWeightRecord {
-  constructor(public date: Date, public weight: number) {
-  }
-}
